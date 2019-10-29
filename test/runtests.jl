dc2d6b1d92142b328a779526efe03f14db08fc27
Noah C. Benson <n@nben.net>
2019-10-28 20:58:26 -0400

    namespace fixes...

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,12 +5,12 @@ using Test
     # Some simple tests of the basic functions for PVec's:
     u = Air.PVec(1:100)
     @test length(u) == 100
-    @test length(dissoc(u, 100)) == 99
-    @test length(assoc(u, 101, 0)) == 101
+    @test length(Air.dissoc(u, 100)) == 99
+    @test length(Air.assoc(u, 101, 0)) == 101
 
     uu = [(1:100)...]
     @test u == uu
-    v = assoc(u, 10, 0)
+    v = Air.assoc(u, 10, 0)
     vv = [(1:100)...]
     vv[10] = 0
     @test v == vv
