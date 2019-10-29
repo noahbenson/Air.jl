63da57cda931d62a2eb90cb26fb084681a51cc62
Noah C. Benson <n@nben.net>
2019-10-28 20:56:18 -0400

    more tests...

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,6 +2,19 @@ using Air
 using Test
 
 @testset "Air.jl" begin
+    # Some simple tests of the basic functions for PVec's:
     u = Air.PVec(1:100)
     @test length(u) == 100
+    @test length(dissoc(u, 100)) == 99
+    @test length(assoc(u, 101, 0)) == 101
+
+    uu = [(1:100)...]
+    @test u == uu
+    v = assoc(u, 10, 0)
+    vv = [(1:100)...]
+    vv[10] = 0
+    @test v == vv
+    @test v[10] == 0
+    @test v[1] == 1
+    @test v[100] == 100
 end
