08f45de5315cf9fd4662a75358c61ebb2d9a03eb
Noah C. Benson <n@nben.net>
2020-05-01 19:24:53 -0700

    updates to some of the tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,49 +5,91 @@ using Random.Random
 @testset "Air.jl" begin
 
     # #PVec and #PMap, simple tests ############################################
-    @testset "PVec" begin
-        # Some simple tests of the basic functions for PVec's:
-        u = Air.PVec(1:100)
-        @test length(u) == 100
-        @test length(Air.dissoc(u, 100)) == 99
-        @test length(Air.assoc(u, 101, 0)) == 101
-
-        uu = [(1:100)...]
-        @test u == uu
-        v = Air.assoc(u, 10, 0)
-        vv = [(1:100)...]
-        vv[10] = 0
-        @test v == vv
-        @test v[10] == 0
-        @test v[1] == 1
-        @test v[100] == 100
-    end
-    @testset "PMap" begin
-        m0 = Air.PMap{Symbol}()
-        pairs = Dict{UInt64,Symbol}(100 => :a, 5 => :b, 25 => :c,
-                                    1 => :d, 0b100001 => :e,
-                                    0b1000001 => :f)
-        (m, n) = (m0, 0)
-        for kv in pairs
-            @test length(m) == n
-            n += 1
-            m = Air.assoc(m, kv[1], kv[2])
+    #@testset "PVec" begin
+    #    # Some simple tests of the basic functions for PVec's:
+    #    u = Air.PVec(1:100)
+    #    @test length(u) == 100
+    #    @test length(Air.dissoc(u, 100)) == 99
+    #    @test length(Air.assoc(u, 101, 0)) == 101
+    #
+    #    uu = [(1:100)...]
+    #    @test u == uu
+    #    v = Air.assoc(u, 10, 0)
+    #    vv = [(1:100)...]
+    #    vv[10] = 0
+    #    @test v == vv
+    #    @test v[10] == 0
+    #    @test v[1] == 1
+    #    @test v[100] == 100
+    #end
+    #@testset "PMap" begin
+    #    m0 = Air.PMap{Symbol}()
+    #    pairs = Dict{UInt64,Symbol}(100 => :a, 5 => :b, 25 => :c,
+    #                                1 => :d, 0b100001 => :e,
+    #                                0b1000001 => :f)
+    #    (m, n) = (m0, 0)
+    #    for kv in pairs
+    #        @test length(m) == n
+    #        n += 1
+    #        m = Air.assoc(m, kv[1], kv[2])
+    #    end
+    #    for kv in pairs
+    #        @test haskey(m, kv[1])
+    #        @test m[kv[1]] === kv[2]
+    #    end
+    #    for kv in m
+    #        @test haskey(pairs, kv[1])
+    #        @test pairs[kv[1]] === kv[2]
+    #    end
+    #    mm = m
+    #    for k in UInt64[0b1000001, 0b100001, 1]
+    #        @test length(mm) == n
+    #        mm = Air.dissoc(mm, k)
+    #        n -= 1
+    #        @test !haskey(mm, k)
+    #    end
+    #end
+    @testset "PArray" begin
+        numops = 100
+        @testset "1D" begin
+            a = Real[]
+            p = Air.PVector{Real}()
+            ops = [:push, :pushfirst, :pop, :popfirst, :set, :get]
+            for ii in 1:numops
+                q = rand(ops)
+                if q == :push
+                    x = rand(Float64)
+                    push!(a, x)
+                    p = Air.push(p, x)
+                elseif q == :pop && length(a) > 0
+                    x = pop!(a)
+                    @test x == p[end]
+                    p = Air.pop(p)
+                elseif q == :pushfirst
+                    x = rand(Float64)
+                    pushfirst!(a, x)
+                    p = Air.pushfirst(p, x)
+                elseif q == :popfirst && length(a) > 0
+                    x = popfirst!(a)
+                    @test x == p[1]
+                    p = Air.popfirst(p)
+                elseif q == :set && length(a) > 0
+                    k = rand(1:length(a))
+                    v = rand(Float64)
+                    a[k] = v
+                    p = Air.setindex(p, v, k)
+                elseif q == :get && length(a) > 0
+                    k = rand(1:length(a))
+                    @test a[k] == p[k]
+                end
+                @test a == p
+                @test size(a) == size(p)
+            end
         end
-        for kv in pairs
-            @test haskey(m, kv[1])
-            @test m[kv[1]] === kv[2]
-        end
-        for kv in m
-            @test haskey(pairs, kv[1])
-            @test pairs[kv[1]] === kv[2]
-        end
-        mm = m
-        for k in UInt64[0b1000001, 0b100001, 1]
-            @test length(mm) == n
-            mm = Air.dissoc(mm, k)
-            n -= 1
-            @test !haskey(mm, k)
-        end
+        #@testset "2D" begin
+        #end
+        #@testset "3D" begin
+        #end
     end
 
     # #PSet ####################################################################
@@ -70,6 +112,7 @@ using Random.Random
             end
         end
     end
+    if false
     let syms = [:a, :b, :c, :d, :e, :f, :g, :h, :i, :j], n = 100
         @testset "PIdSet" begin
             compare_test(Air.PIdSet{Symbol}(), Base.IdSet{Symbol}(), syms, n)
@@ -83,5 +126,6 @@ using Random.Random
             compare_test(Air.PEqualSet{Symbol}(), Set{Symbol}(), syms, n)
             compare_test(Air.PEqualSet{Symbol}([:b, :d, :e]), Set{Symbol}([:b, :d, :e]), syms, n)
         end
+    end
     end
 end
