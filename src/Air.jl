b587ea3562214b37352676df222e389c1ead94b5
noahbenson <nben@uw.edu>
2020-08-07 15:48:43 -0700

    added more exports

--- src/Air.jl
+++ src/Air.jl
@@ -13,6 +13,12 @@ include("TX.jl")
 # Exports; sorted by file:
 #      PArray exports:
 export PList, PSet, PDict, PArray, PVector, PMatrix, LazyDict,
-       push, pop, pushfirst, popfirst, setindex, delete #, insert
+    push, pop, pushfirst, popfirst, setindex, delete, #, insert
+    Mutability, Immutable, Mutable, mutability, isimmtype,
+    isequiv, equivhash, equalfn, hashfn, EquivSet, EquivDict,
+    Var, Volatile, Actor, Source, tx, ReentrantRef,
+    TransactionalRef, AbstractSourceKernel, getfilter,
+    getfinalize, setfilter!, setfinalize!, send, geterror,
+    receive, reset
 
 end # module
