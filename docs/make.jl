b553e82f50118fd2acbc18f1b985306bff46fa88
noahbenson <nben@uw.edu>
2021-10-20 20:17:55 -0700

    fixed the build to work with GitHub Actions (hopefully)

--- docs/make.jl
+++ docs/make.jl
@@ -1,22 +1,25 @@
 using Documenter, Air
 
 makedocs(;
-    modules=[Air],
-    format=Documenter.HTML(),
-    pages=[
-        "Home" => "index.md",
-        "Persistent Dictionaries" => "pdict.md",
-        "Persistent Sets" => "pset.md",
-        "Persistent Arrays" => "parray.md"
-    ],
-    repo="https://github.com/noahbenson/Air.jl/blob/{commit}{path}#L{line}",
-    sitename="Air.jl",
-    authors="Noah C. Benson",
-    #assets=String[],
+         modules=[Air],
+         format=Documenter.HTML(),
+         pages=[
+             "Home" => "index.md",
+             "Persistent Arrays" => "parray.md",
+             "Persistent Dictionaries" => "pdict.md",
+             "Persistent Sets" => "pset.md",
+             "Persistent Weighted Dictionaries" => "pwdict.md",
+             "Persistent Weighted Sets" => "pwset.md",
+             "Persistent Lazy Dictionaries" => "lazydict.md"],
+         repo="https://github.com/noahbenson/Air.jl/blob/{commit}{path}#L{line}",
+         sitename="Air.jl",
+         authors="Noah C. Benson",
+         strict=true,
+         #assets=String[],
 )
 
 deploydocs(;
            repo="github.com/noahbenson/Air.jl",
-           deploy_config=Documenter.Travis(),
+           deploy_config=Documenter.GitHubActions(),
            push_preview=false
 )
